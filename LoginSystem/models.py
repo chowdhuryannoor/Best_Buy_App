761c1b110a59ed527e444ed5922315f82b2a4ee4
chowdhuryannoor <53543972+chowdhuryannoor@users.noreply.github.com>
2020-03-26 19:41:26 -0400

    Just trying to switch branches

--- LoginSystem/models.py
+++ LoginSystem/models.py
@@ -1,15 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
+
+
+class EmployeeType(models.Model):
+    user = models.OneToOneField()
+    type = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.type
 
 
 class Users(models.Model):
     employeeFirst = models.CharField(max_length=25)
     employeeLast = models.CharField(max_length=25)
     employeeId = models.CharField(max_length=8)
-    employeeType = models.TextField()
+    employeeType = models.ForeignKey(EmployeeType, on_delete=models.CASCADE)
     salt = models.TextField()
     password = models.TextField()
     dateCreated = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
 
 
 class Entries(models.Model):
--- LoginSystem/views.py
+++ LoginSystem/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -8,5 +9,13 @@ def login(request):
 
 
 def signup(request):
-    return render(request, 'LoginSystem/signup.html')
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('login')
+    return render(request, 'LoginSystem/signup.html', {'form': form})
 
